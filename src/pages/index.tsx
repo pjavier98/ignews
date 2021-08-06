df35964685944e065a186ce6931cbe520aa3a924
Pedro Javier <pjpc@ic.ufal.br>
2021-08-06 14:13:38 -0300

    feat: add dinamic title per page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
+import { Head } from 'next/document'
 import styles from '../styles/home.module.scss'
 
 export default function Home() {
   return (
+    <>
+    <Head>
+      <title>Inicio | ig.news</title>
+    </Head>
     <h1 className={styles.title}>Hello World</h1>
+    </>
   )
 }
