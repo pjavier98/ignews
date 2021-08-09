8092b63136c022bf831efe15f178510d74790e50
Pedro Javier <pjpc@ic.ufal.br>
2021-08-09 20:15:34 -0300

    feat: insert user on faunadb

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,13 @@
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
 
+import { fauna } from '../../../services/fauna'
+import { query as q } from "faunadb";
+
 export default NextAuth({
+  // jwt: {
+  //   signingKey: process.env.JWT_SIGNING_PRIVATE_KEY,
+  // },
   providers: [
     Providers.GitHub({
       clientId: process.env.GITHUB_CLIENT_ID,
@@ -9,4 +15,24 @@ export default NextAuth({
       scope: 'read:user'
     }),
   ],
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user;
+
+
+
+      try {
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            { data: { email } }
+          )
+        )
+
+        return true;
+      } catch (e) {
+        return false;
+      }
+    }
+  }
 })
