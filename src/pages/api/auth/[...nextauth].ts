b29fb8b0d3bef1bef0028c0df56cfdc79cdb8d39
Pedro Javier <pjpc@ic.ufal.br>
2021-08-09 20:57:03 -0300

    feat: verify if user is not created by email before insert

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,7 +2,7 @@ import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
 
 import { fauna } from '../../../services/fauna'
-import { query as q } from "faunadb";
+import { query as q } from 'faunadb';
 
 export default NextAuth({
   // jwt: {
@@ -19,18 +19,33 @@ export default NextAuth({
     async signIn(user, account, profile) {
       const { email } = user;
 
-
-
       try {
         await fauna.query(
-          q.Create(
-            q.Collection('users'),
-            { data: { email } }
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                   q.Casefold(user.email)
+                ),
+              ),
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email),
+              )
+            )
           )
         )
 
         return true;
       } catch (e) {
+        console.log(e)
         return false;
       }
     }
