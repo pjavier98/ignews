20496004f4209b4a2584a0f974355e74fb4ac54a
Pedro Javier <pjpc@ic.ufal.br>
2021-08-09 11:04:47 -0300

    feat: create header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image'
+import styles from './styles.module.scss';
+
+import logo from '../../../public/images/logo.svg'
+
+export function Header() {
+    return (
+        <header className={styles.headerContainer}>
+            <div className={styles.headerContent}>
+                <Image src={logo} alt="ig.news" />
+                <nav>
+                    <a className={styles.active}>Home</a>
+                    <a>Posts</a>
+                </nav>
+            </div>
+        </header>
+    );
+}
