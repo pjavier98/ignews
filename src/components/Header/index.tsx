980b559927df62f57e57031ff2c31c277b6cccc2
Pedro Javier <pjpc@ic.ufal.br>
2021-08-10 21:25:47 -0300

    feat: menu navigation

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,6 +3,7 @@ import styles from './styles.module.scss';
 
 import logo from '../../../public/images/logo.svg'
 import { SignInButton } from './SignInButton';
+import Link from 'next/link'
 
 export function Header() {
     return (
@@ -10,8 +11,12 @@ export function Header() {
             <div className={styles.headerContent}>
                 <Image src={logo} alt="ig.news" />
                 <nav>
+                  <Link href="/">
                     <a className={styles.active}>Home</a>
+                  </Link>
+                  <Link href="/posts" prefetch>
                     <a>Posts</a>
+                  </Link>
                 </nav>
 
                 <SignInButton />
